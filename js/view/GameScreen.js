7e9de8e158abf456e695f617b178371ff9567d17
Jonathan Olson <jonathan.olson@colorado.edu>
2018-04-11 20:18:14 -0600

    Add mixed numbers flags

--- js/view/GameScreen.js
+++ js/view/GameScreen.js
@@ -30,7 +30,7 @@ define( function( require ) {
     };
 
     Screen.call( this,
-      function() { return new BuildingGameModel(); },
+      function() { return new BuildingGameModel( true ); },
       function( model ) { return new BuildingGameScreenView( model ); },
       options
     );
