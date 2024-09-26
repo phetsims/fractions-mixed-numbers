21a786eb84757c36d6b2128bc398ae7fb0ab3dd1
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:25 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for fractions-mixed-numbers.
+ * ESLint configuration for fractions-mixed-numbers.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
